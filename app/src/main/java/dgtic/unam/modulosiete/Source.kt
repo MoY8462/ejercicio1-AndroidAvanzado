1b2cb1617c1479b5111cc3484d08124bd9152ea7
MoY8462 <moy.sav21@hotmail.com>
2022-12-02 00:17:36 -0600

    chore: 🚀 ejercicio1

---
+++ app/src/main/java/dgtic/unam/modulosiete/Source.kt
@@ -0,0 +1,13 @@
+package dgtic.unam.modulosiete
+
+class Source {
+    companion object{
+        val dataList=listOf<Data>(
+            Data("Dato1","Paisaje 1"),
+            Data("Dato2","Paisaje 2"),
+            Data("Dato3","Paisaje 3"),
+            Data("Dato4","Paisaje 4"),
+            Data("Dato5","Paisaje 5"),
+        )
+    }
+}
